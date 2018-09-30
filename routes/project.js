b311600c4f637052d3efb67590ff988ef1f45d7e
Jordan Stokes <jordan.m.stokes@outlook.com>
2018-09-30 17:58:55 -0500

    handled error thrown in project route

--- routes/project.js
+++ routes/project.js
@@ -10,17 +10,29 @@ const router = Express.Router();
 const projects = data.projects;
 
 //handler for the "/projects path"
-router.get('/:id', (request, response) =>
+router.get('/:id', (request, response, next) =>
 {
     //gets project id from paramater sent in the client's request and retrieves given project
     const id = request.params.id;
     const project = projects[id];
 
-    //sets locals to the data in the "data.json" file
-    response.locals = project;
-    
-    //renders "project.pug" in views folder
-    response.render('project');
+    if(project)
+    {
+        //sets locals to the data in the "data.json" file
+        response.locals = project;
+        
+        //renders "project.pug" in views folder
+        response.render('project');
+    }
+    else
+    {
+        //creates error to throw for missing page
+        const error = new Error("This project doesn't exist: ");
+
+        error.status = 404;
+
+        next(error);
+    }
 });
 
 module.exports = router;
